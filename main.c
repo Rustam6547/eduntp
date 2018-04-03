c4eba99d093a24fa5623f5fae9961e7c04874989
Rustam6547 <37705334+Rustam6547@users.noreply.github.com>
2018-04-03 23:02:56 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char * argv[])
-
+int main(int argc, char *argv[])
 {
-    system("rm //ect/ntp.conf");
-   FILE*fd= fopen("/ect/ntp.conf","w");
+
+    if(argc < 2)
+    {
+        printf("Error: less than 1 args\n");
+        return 0;
+    }
+    system("rm /etc/ntp.conf");
+    FILE *fd = fopen("/etc/ntp.conf", "w");
+
+    if (strcmp(argv[1], "server") == 0)
+    {
+        fprintf(fd, "%s %s\n", "server", argv[2]);
+    }
+    else if (strcmp(argv[1], "client") == 0)
+    {
+        fprintf(fd, "%s %s\n", "restrict", argv[2]);
+    }
 
 
+    else if (strcmp(argv[1], "backup") == 0)
+    {
+
+        system("cp /etc/ntp.conf /etc/ntp.conf.backup");
+    }
+
+
+    else
+    {
+       fprintf(fd, "%s %s\n", "server", "127.127.1.1");
+    }
+
+
+
+
+
+
+    fclose(fd);
+
+    system("systemctl restart ntp");
     return 0;
 }
